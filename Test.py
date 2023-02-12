d3c8cc261d375056dc2d4dca200db98bd51ca7b3
Qubeet <QubeetChannel@gmail.com>
2023-02-12 21:25:46 +0300

    Python

--- Test.py
+++ Test.py
@@ -1,5 +1,35 @@
-import tkinter
 import random
+from tkinter import *
+
+# class Block:
+#     def __init__(self, master, func):
+#         self.ent = Entry(master, width=20)
+#         self.but = Button(master, text="Преобразовать")
+#         self.lab = Label(master, width=20, bg='black', fg='white')
+#         self.but['command'] = getattr(self, func)
+#         self.ent.pack()
+#         self.but.pack()
+#         self.lab.pack()
+#
+#     def str_to_sort(self):
+#         s = self.ent.get()
+#         s = s.split()
+#         s.sort()
+#         self.lab['text'] = ' '.join(s)
+#
+#     def str_reverse(self):
+#         s = self.ent.get()
+#         s = s.split()
+#         s.reverse()
+#         self.lab['text'] = ' '.join(s)
+#
+#
+# root = Tk()
+#
+# first_block = Block(root, 'str_to_sort')
+# second_block = Block(root, 'str_reverse')
+#
+# root.mainloop()
 
 class Color:
     def __init__(self):
@@ -26,11 +56,11 @@ class Color:
                         print("Числа не могут быть меньше 0 или больше 255.\n")
                     else:
                         self.red_10 = rgb[0]
-                        self.red_16 = convert_to(rgb[0], 16)
+                        self.red_16 = convert_to_16(rgb[0])
                         self.green_10 = rgb[1]
-                        self.green_16 = convert_to(rgb[1], 16)
+                        self.green_16 = convert_to_16(rgb[1])
                         self.blue_10 = rgb[2]
-                        self.blue_16 = convert_to(rgb[2], 16)
+                        self.blue_16 = convert_to_16(rgb[2])
                         self.HTML = f"{self.red_16}{self.green_16}{self.blue_16}"
 
                         print("RGB-код готов!")
@@ -38,44 +68,47 @@ class Color:
             else:
                 print("Вы ввели не то количество аргументов, введите 3 числа.\n")
 
-# def convert_to(number, base, upper=False):
-#     digits = '0123456789abcdefghijklmnopqrstuvwxyz'
-#     if base > len(digits):
-#         return None
-#     result = ''
-#     while number > 0:
-#         result = digits[number % base] + result
-#         number //= base
-#     return result.upper() if upper else result
-
-
-
-
-def convert_to(number, base):
+def convert_to_16(number):
     digits = "0123456789abcdef"
     result = ""
-    if number < base:
-        result = "0" + digits[number % base]
+    if number < 16:
+        result = "0" + digits[number % 16]
     else:
         while number > 0:
-            result = digits[number % base] + result
-            number //= base
+            result = digits[number % 16] + result
+            number //= 16
     return result.upper()
 
-def convert_from(number, base):
+def convert_to_10(number):
     digits = "0123456789abcdef"
     for i in number:
-        if i.lower() not in digits[0:base]:
+        if i.lower() not in digits[0:10]:
             return None
 
 
+def convert_to(number, base, upper=False):
+    digits = '0123456789abcdefghijklmnopqrstuvwxyz'
+    if base > len(digits):
+        return None
+    result = ''
+    while number > 0:
+        result = digits[number % base] + result
+        number //= base
+    return result.upper() if upper else result
 
-#
-# rgb_start = Color()
+
+
+
+
+
+
+
+
+rgb_start = Color()
 # rgb_start.get_color()
 # rgb_end.get_color()
-# print(f"R10 - {rgb_start.red_10}, G10 - {rgb_start.green_10}, B10 - {rgb_start.blue_10}")
-# print(f"R16 - {rgb_start.red_16} \nG16 - {rgb_start.green_16} \nB16 - {rgb_start.blue_16}")
-# print(f"{rgb_start.HTML}")
+print(f"R10 - {rgb_start.red_10}, G10 - {rgb_start.green_10}, B10 - {rgb_start.blue_10}")
+print(f"R16 - {rgb_start.red_16} \nG16 - {rgb_start.green_16} \nB16 - {rgb_start.blue_16}")
+print(f"{rgb_start.HTML}")
 # digit = "0123456789"
 # print(digit[0:5])
